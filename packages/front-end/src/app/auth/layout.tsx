fd26abffa31fa8ecf39ce6cda8a4971ba0d84dd1
Mees van Wel <mees.vanwel@hexa-it.nl>
2023-11-27 21:45:33 +0100

    Implemented auth layout Implemented eurostile font Implemented colors Implemented Icons Fixed ignore local env file back-end

---
+++ packages/front-end/src/app/auth/layout.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import Link from "next/link";
+import background from "@/assets/images/bg.jpeg";
+import icon from "@/assets/images/icon-white.svg";
+import styles from "./layout.module.scss";
+import { Button, Group, Stack, Title } from "@mantine/core";
+import { useCompanyName } from "@/hooks/useCompanyName";
+import { IconQuestionMark } from "@tabler/icons-react";
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const companyName = useCompanyName();
+
+  return (
+    <>
+      <Image
+        alt="Background"
+        src={background}
+        placeholder="blur"
+        quality={100}
+        fill
+        sizes="100vw"
+        className={styles.background}
+      />
+      <main className={styles.main}>
+        <aside className={styles.aside}>
+          <Group wrap="nowrap">
+            <Image alt="Icon" src={icon} width={64} height={64} />
+            <Stack gap={0}>
+              <Title>Hexa Center</Title>
+              {companyName && <Title order={6}>{companyName}</Title>}
+            </Stack>
+          </Group>
+          <div className={styles.content}>{children}</div>
+          <Stack align="center">
+            <Button
+              component={Link}
+              variant="light"
+              leftSection={<IconQuestionMark size={16} />}
+              size="sm"
+              href="https://www.hexa.center/guide/system/login/"
+              target="_blank"
+            >
+              Help
+            </Button>
+            <p> Version 1.3.0 (Blue Heron)</p>
+          </Stack>
+        </aside>
+        <div className={styles.gradient} />
+      </main>
+    </>
+  );
+}
